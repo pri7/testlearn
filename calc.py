f4aad0546d58d9b820e7a423417c0d2a615438e3
Priyanshu Piyush <codedrog@gmail.com>
2018-09-27 10:16:20 +0000

    first

---
+++ calc.py
@@ -0,0 +1,15 @@
+def add(x,y):
+	"""Adding Function"""
+	return x+y
+
+def subtract(x,y):
+	"""Subtraction Function"""
+	return x-y
+
+def multiply(x,y):
+	"""Multiplication Function"""
+	return x*y
+
+def divide(x,y):
+	"""Division Function"""
+	return x/y
